14e52b97df2afd19a6718c9844d1383f8d027264
narut-o <akash_ranjan__@outlook.com>
2025-12-07 07:33:30 +0530

    Fix errorHandler path for Render

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,4 @@
-import ErrorHandler from "../backend/utils/errorHandler.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 const errorMiddleWare = (err,req,res,next)=>{
     err.statusCode = err.statusCode||500;
